8fe7cd6b507875e260949383709ca5bdb5f3d439
Arthur Maurício Rodrigues <64982033+arthurmrodr@users.noreply.github.com>
2022-12-07 21:51:56 -0300

    improved data and ran first synth model

--- data_treatment.R
+++ data_treatment.R
@@ -7,36 +7,53 @@ library(dplyr)
 
 list_series <- ipeadatar::available_series()
 
-# Baixando séries relevantes
+##### Baixando séries relevantes
 
 gini <- 
   ipeadata("PGINI") %>% 
-  filter(uname == "States")
+  filter(uname == "States") # Gini
 gini <- gini %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(gini = value)
 
 rendad_pc <-
   ipeadata("MRDPC") %>% 
-  filter(uname == "States")  
+  filter(uname == "States") # Renda per capita  
 rendad_pc <- rendad_pc %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(rendad_pc = value)
 
 desemp <-
   ipeadata("Dese_Pnad") %>% 
-  filter(uname == "States")  
+  filter(uname == "States") # Taxa de desemprego 
 desemp <- desemp %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(desemp = value)
 
 pop_tot <- ipeadata("ESTIMA_PO") %>% 
-  filter(uname == "States")
+  filter(uname == "States") # População total
 pop_tot <- pop_tot %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(pop_tot = value)
 
 homic <- ipeadata("THOMIC") %>% 
-  filter(uname == "States")
+  filter(uname == "States") # Taxa de homicídio
 homic <- homic %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(homic = value)
 
+pop_urb <- ipeadata("POPUR") %>% 
+  filter(uname == "States") # População urbana
+pop_urb <- pop_urb %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(popurb = value)
+
+freqesc <- ipeadata("SGR1517") %>% 
+  filter(uname == "States") # Frequência Escolar (15 a 17 anos)
+freqesc <- freqesc %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(freqesc = value)
+
+pop1519m <- ipeadata("POP15_19M") %>% 
+  filter(uname == "States") # População masculina 15 a 19 anos
+pop1519m <- pop1519m %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(pop1519m = value)
+
+pop2024m <- ipeadata("POP20_24M") %>% 
+  filter(uname == "States") # População masculina 20 a 24 anos
+pop2024m <- pop2024m %>% mutate(ano = substr(date, 1, 4)) %>% mutate(id = paste(tcode, "_", ano, sep = "")) %>% rename(pop2024m = value)
 
 # Tratando bases de dados
 
-df_join <- reduce(list(desemp, gini, rendad_pc, pop_tot, homic),
+df_join <- reduce(list(desemp, gini, rendad_pc, pop_tot, homic, pop_urb, freqesc, pop1519m, pop2024m),
                   ~.x %>%
-                    left_join(.y, by="id"))
+                    full_join(.y, by="id"))
+
+df_join <- df_join %>% mutate(ano = substr(id, 4, 7))
 
 
---
+++ synth_model.R
@@ -0,0 +1,84 @@
+library(Synth)
+library(caret)
+library(tidyselect)
+library(plm)
+
+# Data preparation
+
+df_join$ano <- as.numeric(df_join$ano)
+df_join <- df_join %>% filter(ano >= 1992 & ano <= 2014)
+df_select <- df_join %>% select(id, tcode, ano, desemp, gini, rendad_pc, pop_tot, homic, popurb, freqesc, pop1519m, pop2024m)
+df_select <- df_select %>% mutate(tcode = substr(id, 1, 2))
+
+
+
+
+df_select <- df_select %>%
+  group_by(tcode) %>%
+  mutate(homic = ifelse(is.na(homic), (dplyr::lag(homic) + dplyr::lead(homic)) / 2, homic)) # Caso não exista dado de homicídio, considera-se a média do ano anterior e seguinte
+
+df_select <- df_select %>% mutate(pct_urb = popurb / pop_tot,
+                                  pct_jov = (pop1519m + pop2024m) / pop_tot,
+                                  log_rpc = log(rendad_pc)) # Gerando as variáveis necessárias
+
+
+df_select$tcode <- as.numeric(df_select$tcode)
+
+df_select <- as.data.frame(df_select)
+
+
+# Rodando Controle Sintético
+
+dataprep_out <- dataprep(foo = df_select,
+                         predictors = c("desemp", "gini", "log_rpc", "pct_urb", "pct_jov", "freqesc"),
+                         predictors.op = "mean",
+                         time.predictors.prior = c(1992:2006),
+                         dependent = "homic",
+                         unit.variable = "tcode",
+                         time.variable = "ano",
+                         treatment.identifier = 26,
+                         controls.identifier = c(11, 12, 13, 14, 15, 16, 17,
+                                                 21, 22, 23, 24, 25, 27, 28, 29,
+                                                 31, 32, 33, 35,
+                                                 41, 42, 43,
+                                                 50, 51, 52, 53),
+                         time.optimize.ssr = c(1992:2007),
+                         time.plot = c(1992:2014))
+
+synth_out = dataprep_out %>% synth()
+
+synth_out %>% path.plot(dataprep.res = dataprep_out, tr.intake = 2007)
+
+
+synth_control = dataprep_out$Y0plot %*% synth_out$solution.w
+
+
+print(synth.tables   <- synth.tab(
+  dataprep.res = dataprep_out,
+  synth.res    = synth_out)
+)
+
+
+
+gaps.plot(synth_out, dataprep_out)
+
+
+placebos <- generate.placebos(dataprep_out, synth_out, Sigf.ipop = 3)
+
+
+
+#df_complete <- df_join# %>% complete(tcode, ano = 1992:2014)
+
+#df_complete$homic <- as.numeric(df_complete$homic)
+#df_complete <- make.pbalanced(df_complete, balance.type = "fill")
+
+#df_train <- df_complete %>% select(homic, ano, tcode)
+#df_train <- df_train %>% pivot_longer(df_train, cols = c(homic), names_to = c("tcode", "ano"), values_to = "homic")
+
+#model <- train(df_complete, method = "randomForest")
+
+#weights <- synth(df_complete, weights = c(12, 27, 16, 13, 29, 23, 53, 32,
+#                                          52, 21, 51, 50, 31, 15, 25, 41,
+#                                          22, 24, 43, 11, 14, 42, 28,
+#                                          17))
+#print()
