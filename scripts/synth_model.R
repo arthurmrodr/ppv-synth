7660aa37bebdfcf06dda9c572d553a46e8ea2a1f
Arthur Maurício Rodrigues <64982033+arthurmrodr@users.noreply.github.com>
2022-12-16 22:38:32 -0300

    descriptive table

--- scripts/synth_model.R
+++ scripts/synth_model.R
@@ -5,6 +5,8 @@ library(plm)
 library(fixest)
 library(did)
 library(stargazer)
+library(SCtools)
+library(table1)
 
 
 # Reading dataframe generated in "data_treatment.R"
@@ -31,6 +33,15 @@ df_select$tcode <- as.numeric(df_select$tcode)
 
 df_select <- as.data.frame(df_select)
 
+df_select <- df_select %>% mutate(tratamento = ifelse(
+  (ano >= 2003 & tcode == 31) |
+    (ano >= 2007 & tcode == 26) |
+    (ano >= 2007 & tcode == 33) |
+    (ano >= 2011 & tcode == 32) |
+    (ano >= 2011 & tcode == 25),
+  1, 0
+))
+
 #Base para controle sintético padronizado
 
 df_standardized <- df_select %>% mutate_at(vars(desemp, gini, rendad_pc, homic, freqesc, pct_jov, pct_urb, log_rpc, tsuicid, desp_pc), scale)
@@ -39,18 +50,27 @@ df_standardized <- as.data.frame(df_standardized)
 
 #Base para Callaway e Sant'Anna
 
-df_cs <- df_select %>% mutate(tratamento = ifelse(
-  (ano >= 2003 & tcode == 31) |
-  (ano >= 2007 & tcode == 26) |
-  (ano >= 2007 & tcode == 33) |
-  (ano >= 2011 & tcode == 32) |
-  (ano >= 2011 & tcode == 25),
-  1, 0
-))
-df_cs <- df_cs %>%  group_by(tcode) %>% mutate(anos_pra_tratar = ano - ano[which(tratamento == 1)[1]])
+
+df_cs <- df_select %>%  group_by(tcode) %>% mutate(anos_pra_tratar = ano - ano[which(tratamento == 1)[1]])
 df_cs$anos_pra_tratar <- df_cs$anos_pra_tratar %>% replace_na(0)
 df_cs <- df_cs %>% group_by(tcode) %>% mutate(ano_tratamento = ano[which(tratamento == 1)[1]])
 df_cs$ano_tratamento <- df_cs$ano_tratamento %>% replace_na(0)
+
+#### Tabela Descritiva ####
+
+table1::label(df_select$desemp) <- "Taxa de Desemprego (%)"
+table1::label(df_select$gini) <- "Coeficiente de Gini"
+table1::label(df_select$rendad_pc) <- "Renda média domiciliar per capita"
+table1::label(df_select$homic) <- "Taxa de Homicídio (por 100.000 hab.)"
+table1::label(df_select$freqesc) <- "Frequência Escolar - 15 a 17 anos (%)"
+table1::label(df_select$tsuicid) <- "Taxa de Suicídio (por 100.000 hab.)"
+table1::label(df_select$desp_pc) <- "Despesa segurança do gov. est. per capita"
+table1::label(df_select$pct_urb) <- "População em área urbana (%)"
+table1::label(df_select$pct_jov) <- "População masculina entre 15 e 24 anos (%)"
+
+
+table1(~desemp+gini+rendad_pc+
+         homic+freqesc+tsuicid+desp_pc+pct_urb+pct_jov, data = df_select)
 
 #### Synthetic Control ####
 
@@ -99,6 +119,9 @@ gaps.plot(synth_m1, dataprep_m1) #gaps plot
 
 placebos <- generate.placebos(dataprep_m1, synth_m1, Sigf.ipop = 3) # placebo exercise
 
+plot_placebos(placebos)
+
+mspe.plot(placebos, discard.extreme = TRUE, mspe.limit = 1, plot.hist = TRUE)
 
 # Com variável de homicídio para 2006
 
@@ -209,7 +232,7 @@ summary(reg_plain)
 
 reg_controls <- plm(homic ~ tratamento  + gini + desemp + log_rpc  , data = df_twfe)
 summary(reg_controls)
-
+stargazer(reg_controls, type = "latex")
 #event study sem controles
 
 twfe_1 <- fixest::feols(homic ~
@@ -232,7 +255,7 @@ cs_homic <- att_gt(yname = "homic",
                      tname = "ano",
                      idname = "tcode",
                      gname = "ano_tratamento",
-                     data = df_select,
+                     data = df_cs,
                      xformla = ~1,
                      control_group = "notyettreated")
 
